0d9d0e7143a264d1d912aa2b0b602bea6a6beebc
mcc <andi.m.mcclure@gmail.com>
2021-04-01 19:57:28 -0400

    Phonon only

--- main.c
+++ main.c
@@ -1,24 +1,7 @@
 #include <stdio.h>
-#include "OVR_Audio.h"
 #include "phonon.h"
 
 int main() {
-	ovrAudioContextConfiguration config = {};
-    
-    config.acc_Size = sizeof( config );
-    config.acc_SampleRate = 48000;
-    config.acc_BufferLength = 512;
-    config.acc_MaxNumSources = 16;
-    
-    ovrAudioContext ovrContext;
-    
-    if ( ovrAudio_CreateContext( &ovrContext, &config ) != ovrSuccess )
-    {
-      printf( "Failed to initialize Oculus Audio\n" );
-      return 1; 
-    }
-    printf( "Initialized Oculus Audio\n" );
-
     IPLhandle phononContext;
     IPLerror status;
     status = iplCreateContext(NULL, NULL, NULL, &phononContext);
