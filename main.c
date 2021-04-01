9eb6d4f6a947635d40ed884b5586da02605da142
mcc <andi.m.mcclure@gmail.com>
2021-04-01 16:30:54 -0400

    Steam works ovraudio doesn't

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "OVR_Audio.h"
+#include "phonon.h"
 
 int main() {
 	ovrAudioContextConfiguration config = {};
@@ -9,13 +10,22 @@ int main() {
     config.acc_BufferLength = 512;
     config.acc_MaxNumSources = 16;
     
-    ovrAudioContext context;
+    ovrAudioContext ovrContext;
     
-    if ( ovrAudio_CreateContext( &context, &config ) != ovrSuccess )
+    if ( ovrAudio_CreateContext( &ovrContext, &config ) != ovrSuccess )
     {
-      printf( "WARNING: Could not create context!\n" );
+      printf( "Failed to initialize Oculus Audio\n" );
       return 1; 
     }
+    printf( "Initialized Oculus Audio\n" );
+
+    IPLhandle phononContext;
+    IPLerror status;
+    status = iplCreateContext(NULL, NULL, NULL, &phononContext);
+    if (status != IPL_STATUS_SUCCESS) {
+      printf( "Failed to initialize Steam Audio\n" );
+    }
+    printf( "Initialized Steam Audio\n" );
 
 	return 0;
 }
